985c5840b52e26ab10b5d59089f880ea3e792c42
origami <origami1024@gmail.com>
2019-11-04 21:14:10 +0300

    4

--- assets/pace/paceExtv2.js
+++ assets/pace/paceExtv2.js
@@ -858,7 +858,28 @@
     eventLag: EventLagMonitor
   };
 
+  //99% stuck fix
+  var initDestroyTimeOutPace = function() {
+    var counter = 0;
+    console.log('99fixinit')
+    var refreshIntervalId = setInterval( function(){
+        var progress; 
 
+        if( typeof document.querySelector('.pace-progress').dataset.progressText !== 'undefined' ) {
+            progress = Number( document.querySelector('.pace-progress').dataset.progressText.replace("%" ,'') );
+        }
+
+        if( progress === 99 ) {
+            counter++;
+        }
+
+        if( counter > 50 ) {
+            clearInterval(refreshIntervalId);
+            Pace.stop();
+        }
+    }, 100);
+  }
+  initDestroyTimeOutPace();
 
 
   (init = function() {
