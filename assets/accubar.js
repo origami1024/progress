90d26278c6bf1abd4a8cf20e9bfbb1c1c79a05f4
origami <origami1024@gmail.com>
2019-11-08 01:05:23 +0300

    feat: start documentation; minor refactoring

--- assets/accubar.js
+++ assets/accubar.js
@@ -1,78 +1,80 @@
 //TODOS: 
-
 //D. delete the bar element in the end. (leave it now for debugging)
 //E. THINK HOW TO EMBED SVG BEST
 //E.1--svg inside svg
 //E.1.2. will it laod before all other pics?
 
+//////////////
+// Скрипт нужно помещать перед head
+//////////////
+// Чтобы поменять svg на загрузке нужно в presets:
+// 1. изменить picPath
+// 2. изменить svgWidth и svgHeight, на ПРОПОРЦИОНАЛЬНЫЕ тем, что в свг viewBox; пропорционально, чтобы свгшка правильно сжималась/растягивалась по ширине и высоте
+// 3. установить grayscaleBGCopy в нужное значение - true/false
+// 
+//////////////
 
-
-var presets = {
-  presetImagesCount: 0,//reset on interactive, so no accidentaly endless waiting
-  fakeProgressPoints: 0,//each added as setTimer callback after the complete loading of doc, dont implement it for now
-  svgWidth: 142,
-  ghostTime: 1750,//WHEN 100% reached, hide the bar after this time
-  useCssTransition: true,//add value to css transition or set x position manually in js
-  picPath: 'assets/img/progress_v4.svg',
-  picPath: 'assets/img/visa-5.svg',//
-  picPathGray: 'assets/img/visa-5Gray.svg',//
+//////////
+// presets - объект пользовательских настроек
+//////////
+let presets = {
+  svgWidth: 142, //Длина(ее пропорция) viewBox в сторонней свгшке
+  svgHeight: 62, //Высота(ее пропорция) viewBox в сторонней свгшке
+  grayscaleBGCopy: true, //Нужна ли серая копия свгшки бэкграундом?
+  ghostTime: 1750, //"Время в лимбе": после окончания загрузки и последней анимации, анимация скрытия враппера accubar(сейчас opacity в 0) будет длится столько ms
+  useCssTransition: true,//Если true - по прогрессу меняется css переменная, которая указана в transform: translate, что обеспечивает плавную анимацию; если false, то просто двигать координату x в части маски в svg
+  picPath: 'assets/img/progress_v5.svg',//путь к сторонней свгшке
+  //picPath: 'assets/img/visa-5.svg',
 }
-var svgCode = `
-<svg class="inner-svg" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 142 61">
+
+
+//Время начала работы скрипта/приблизительное время начала загрузки страницы
+let startTime = new Date().getTime()
+
+
+/////////////////
+// Дальше идет свгшка-враппер(шаблонная часть), в которой реализованы следующие идеи:
+// 1. Маска из двух прямоугольников на всю область - один белый - показать всё, второй черный, который будет двигаться при анимации
+// 2. Сторонняя основная свгшка подгружается из файла, по пути из presets.picPath
+// 3. Если presets.grayscaleBGCopy задан true, то свгшка вставляется 2 раз с чернобелым фильтром - как фон
+// 4. Свгшка и маска растянуты на весь viewBox, и изменяются через presets.svgWidth, presets.svgHeight
+/////////////////
+let svgCode = `
+<svg class="inner-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${presets.svgWidth} ${presets.svgHeight}">
   <defs>
     <mask id="myMask" x="0" y="0">
-      <rect class="maskPart1" x="0" y="0" width="142" height="62" fill="white"></rect>
-      <rect class="maskMovingPart maskLeftToRight" x="0" y="0" width="142" height="62" fill="black"></rect>
+      <rect class="maskPart1" x="0" y="0" width="${presets.svgWidth}" height="${presets.svgHeight}" fill="white"></rect>
+      <rect class="maskMovingPart maskLeftToRight" x="0" y="0" width="${presets.svgWidth}" height="${presets.svgHeight}" fill="black"></rect>
     </mask>
   </defs>
-  <image style="filter: grayscale(1)" xlink:href="${presets.picPath}" x="0" y="0" preserveAspectRatio="none" width="142" height="62" />
-  <image mask="url(#myMask)" xlink:href="${presets.picPath}" x="0" y="0" preserveAspectRatio="none" width="142" height="62" />
-  
+  ${presets.grayscaleBGCopy ? `<image style="filter: grayscale(1)" xlink:href="${presets.picPath}" x="0" y="0" preserveAspectRatio="none" width="${presets.svgWidth}" height="${presets.svgHeight}" />` : ''}
+  <image mask="url(#myMask)" xlink:href="${presets.picPath}" x="0" y="0" preserveAspectRatio="none" width="${presets.svgWidth}" height="${presets.svgHeight}" />
 </svg>
 `
 
-/*
-<g fill="gray">
-  <path d="M63.62,38.25a10.23,10.23,0,0,0-4.89,1.3V28.91L53,30.18V59.84A17.45,17.45,0,0,0,61.1,62c5.78,0,11.23-3.33,11.23-12.15S67.57,38.25,63.62,38.25ZM61.09,59.07a7.93,7.93,0,0,1-2.36-.37V41.81a6.07,6.07,0,0,1,3.1-.49c1.57.15,4.61,1.43,4.61,8.78S63.43,59.07,61.09,59.07Z"/>
-  <path d="M87.94,38.25a10.19,10.19,0,0,0-4.89,1.3V28.91l-5.74,1.27V59.84A17.44,17.44,0,0,0,85.41,62c5.78,0,11.24-3.33,11.24-12.15S91.89,38.25,87.94,38.25ZM85.41,59.07a7.93,7.93,0,0,1-2.36-.37V41.81a6,6,0,0,1,3.09-.49c1.58.15,4.61,1.43,4.61,8.78S87.74,59.07,85.41,59.07Z"/>
-  <path d="M40.36,57.41a4.35,4.35,0,0,1-2.9,1.29c-4.29.36-4.12-4.7-4.12-4.7V37.65l-5.75,1.27v15s-.33,8,7.45,8a9.73,9.73,0,0,0,6.23-2l.61,1.1h4.23V37.65l-5.75,1.27Z"/>
-  <polygon points="14.93 43.63 5.93 43.63 5.93 28.21 0 29.52 0 61.03 5.93 61.03 5.93 46.98 14.93 46.98 14.93 61.03 20.86 61.03 20.86 28.21 14.93 29.52 14.93 43.63"/>
-  <path d="M131.19,33.43l9-18.9a10,10,0,1,0-17.94,0Zm0-30A6.64,6.64,0,1,1,124.56,10,6.64,6.64,0,0,1,131.19,3.4Z"/>
-  <path d="M131.2,14.76A4.73,4.73,0,0,0,135.92,10a5.36,5.36,0,0,0-.06-.69,2.82,2.82,0,0,1-4.76-2,2.77,2.77,0,0,1,.79-1.94,4,4,0,0,0-.69-.06,4.73,4.73,0,1,0,0,9.45Z"/>
-  <path d="M112.89,38.38a22.08,22.08,0,0,0-4.42-.32,22.51,22.51,0,0,0-3.15.27,20.34,20.34,0,0,0-2.81.63l-.61,2.25h5.57a6.54,6.54,0,0,1,3.82.9c.93.6,1.4,1.8,1.4,3.6v1.8c-.63-.12-1.3-.23-2-.31a15.75,15.75,0,0,0-2.2-.14,17.48,17.48,0,0,0-3.28.29,6.41,6.41,0,0,0-2.58,1.11,5.35,5.35,0,0,0-1.71,2.2,9,9,0,0,0-.62,3.6,9,9,0,0,0,.62,3.6,5.6,5.6,0,0,0,1.66,2.2A6,6,0,0,0,105,61.17a14.84,14.84,0,0,0,3,.29,8.66,8.66,0,0,0,3.38-.56,8.52,8.52,0,0,0,2.08-1.24h.25l.74,1.35H118V45.71a9.87,9.87,0,0,0-.65-3.85A6.12,6.12,0,0,0,112.89,38.38Zm-.2,19a5.38,5.38,0,0,1-3.14.9,5.05,5.05,0,0,1-1.32-.18,2.8,2.8,0,0,1-1.19-.66,3.52,3.52,0,0,1-.87-1.26,4.83,4.83,0,0,1-.34-2,4.92,4.92,0,0,1,.34-2A3.71,3.71,0,0,1,107,51a2.89,2.89,0,0,1,1.19-.65,8.54,8.54,0,0,1,1.73-.18h2.73Z"/>
-  <path d="M138,40.72a6.88,6.88,0,0,0-3-2,13,13,0,0,0-8,0,6.19,6.19,0,0,0-2.75,2,9.49,9.49,0,0,0-1.84,3.62,19.69,19.69,0,0,0-.64,5.42,18.86,18.86,0,0,0,.67,5.42,8.57,8.57,0,0,0,2,3.62,7.21,7.21,0,0,0,3.27,2,17.51,17.51,0,0,0,5,.63,22.85,22.85,0,0,0,3.59-.27,19.91,19.91,0,0,0,2.86-.63l-.62-2.25h-4.84a6.31,6.31,0,0,1-4.69-1.55c-1-1-1.51-3-1.51-5.63h12.9V49.76a19.66,19.66,0,0,0-.65-5.42A9.63,9.63,0,0,0,138,40.72Zm-10.42,7.47a19.56,19.56,0,0,1,.27-3.6,7.62,7.62,0,0,1,.72-2.21A2.76,2.76,0,0,1,131.09,41a2.85,2.85,0,0,1,2.69,1.39,8,8,0,0,1,.71,2.21,18.61,18.61,0,0,1,.28,3.6Z"/>
-  </g>
-  <g class="color1" mask="url(#myMask)">
-  <path d="M63.62,38.25a10.23,10.23,0,0,0-4.89,1.3V28.91L53,30.18V59.84A17.45,17.45,0,0,0,61.1,62c5.78,0,11.23-3.33,11.23-12.15S67.57,38.25,63.62,38.25ZM61.09,59.07a7.93,7.93,0,0,1-2.36-.37V41.81a6.07,6.07,0,0,1,3.1-.49c1.57.15,4.61,1.43,4.61,8.78S63.43,59.07,61.09,59.07Z"/>
-  <path d="M87.94,38.25a10.19,10.19,0,0,0-4.89,1.3V28.91l-5.74,1.27V59.84A17.44,17.44,0,0,0,85.41,62c5.78,0,11.24-3.33,11.24-12.15S91.89,38.25,87.94,38.25ZM85.41,59.07a7.93,7.93,0,0,1-2.36-.37V41.81a6,6,0,0,1,3.09-.49c1.58.15,4.61,1.43,4.61,8.78S87.74,59.07,85.41,59.07Z"/>
-  <path d="M40.36,57.41a4.35,4.35,0,0,1-2.9,1.29c-4.29.36-4.12-4.7-4.12-4.7V37.65l-5.75,1.27v15s-.33,8,7.45,8a9.73,9.73,0,0,0,6.23-2l.61,1.1h4.23V37.65l-5.75,1.27Z"/>
-  <polygon points="14.93 43.63 5.93 43.63 5.93 28.21 0 29.52 0 61.03 5.93 61.03 5.93 46.98 14.93 46.98 14.93 61.03 20.86 61.03 20.86 28.21 14.93 29.52 14.93 43.63"/>
-  <path d="M131.19,33.43l9-18.9a10,10,0,1,0-17.94,0Zm0-30A6.64,6.64,0,1,1,124.56,10,6.64,6.64,0,0,1,131.19,3.4Z"/>
-  <path d="M131.2,14.76A4.73,4.73,0,0,0,135.92,10a5.36,5.36,0,0,0-.06-.69,2.82,2.82,0,0,1-4.76-2,2.77,2.77,0,0,1,.79-1.94,4,4,0,0,0-.69-.06,4.73,4.73,0,1,0,0,9.45Z"/>
-  <path d="M112.89,38.38a22.08,22.08,0,0,0-4.42-.32,22.51,22.51,0,0,0-3.15.27,20.34,20.34,0,0,0-2.81.63l-.61,2.25h5.57a6.54,6.54,0,0,1,3.82.9c.93.6,1.4,1.8,1.4,3.6v1.8c-.63-.12-1.3-.23-2-.31a15.75,15.75,0,0,0-2.2-.14,17.48,17.48,0,0,0-3.28.29,6.41,6.41,0,0,0-2.58,1.11,5.35,5.35,0,0,0-1.71,2.2,9,9,0,0,0-.62,3.6,9,9,0,0,0,.62,3.6,5.6,5.6,0,0,0,1.66,2.2A6,6,0,0,0,105,61.17a14.84,14.84,0,0,0,3,.29,8.66,8.66,0,0,0,3.38-.56,8.52,8.52,0,0,0,2.08-1.24h.25l.74,1.35H118V45.71a9.87,9.87,0,0,0-.65-3.85A6.12,6.12,0,0,0,112.89,38.38Zm-.2,19a5.38,5.38,0,0,1-3.14.9,5.05,5.05,0,0,1-1.32-.18,2.8,2.8,0,0,1-1.19-.66,3.52,3.52,0,0,1-.87-1.26,4.83,4.83,0,0,1-.34-2,4.92,4.92,0,0,1,.34-2A3.71,3.71,0,0,1,107,51a2.89,2.89,0,0,1,1.19-.65,8.54,8.54,0,0,1,1.73-.18h2.73Z"/>
-  <path d="M138,40.72a6.88,6.88,0,0,0-3-2,13,13,0,0,0-8,0,6.19,6.19,0,0,0-2.75,2,9.49,9.49,0,0,0-1.84,3.62,19.69,19.69,0,0,0-.64,5.42,18.86,18.86,0,0,0,.67,5.42,8.57,8.57,0,0,0,2,3.62,7.21,7.21,0,0,0,3.27,2,17.51,17.51,0,0,0,5,.63,22.85,22.85,0,0,0,3.59-.27,19.91,19.91,0,0,0,2.86-.63l-.62-2.25h-4.84a6.31,6.31,0,0,1-4.69-1.55c-1-1-1.51-3-1.51-5.63h12.9V49.76a19.66,19.66,0,0,0-.65-5.42A9.63,9.63,0,0,0,138,40.72Zm-10.42,7.47a19.56,19.56,0,0,1,.27-3.6,7.62,7.62,0,0,1,.72-2.21A2.76,2.76,0,0,1,131.09,41a2.85,2.85,0,0,1,2.69,1.39,8,8,0,0,1,.71,2.21,18.61,18.61,0,0,1,.28,3.6Z"/>
-  </g>
-*/
-var composite = {
-  realProgress: 0, //measured in points, convert when drawing
-  //animatedProgress: 0, //should be less than real progress most of the time
-  progressCap: 3, //maximum points to track, like 1 point per pic, 3 point on interactive state change
-  
-  documentInteractive: 0,
-  documentComplete: 0,
-  doneImages: 0,
-  imageCollection: [],
-  images: [],
-  imageTimeStamps: [], //saving timestamp on image completion
 
-  //elements,?
-  //ajax?
+////////////
+// composite - объект с внутренними состояниями/моделью данных (в основном о текущем состоянии загрузки)
+////////////
+let composite = {
+  realProgress: 0, //фактический(в противовес фейковому/анимированному) прогресс, измеряемый в условных единицах; 2 единицы каждое изображение, 3 и 2 единицы на изменениях documentreadystate
+  progressCap: 5, //максимальный прогресс, когда документ догружается до interactive, определяется кол-во изображений и это значение пересчитывается, так что realProgress в конце будет равен progressCap и делением этих двух получаем текущий фактический процент загрузки
+  documentInteractive: 0, //догрузился ли док-т до Interactive
+  documentComplete: 0, //догрузился ли док-т до Complete
+  doneImages: 0, //кол-во изображений, которые либо завершили загрузку, либо на них ошибка загрузки
+  imageCollection: [], //html-коллекция, с ссылками на все img на странице
+  images: [], //массив значения которого - ИНДЕКСЫ в composite.imageCollection, нужен чтобы знать какие именно изображения еще не загружены и их нужно проверять на таймере. Скорее всего, тут есть потенциал для оптимизации
+  imageTimeStamps: [], //сюда записываются значения времени окончания загрузки каждого изображания по мере их загрузки
+  mask: undefined, //ссылка на двигающийся элемент в свг-враппере
+  //ajax? - слежка за аякс запросами не реализована, потенциально может быть нужно, реализовано в PACE
+  //scripts!!!!!!!!!!!!!!!!!!
+  //css files!!!!!!!!!!!!!!!!
 }
 
-var startTime = new Date().getTime()
-document.documentElement.style.setProperty('--ghostTime', presets.ghostTime + 'ms')
 
-var tracker = setInterval(()=>{
+
+
+let tracker = setInterval(()=>{
   //image.complete tracking
   for (let i = 0, icount=composite.images.length; i < icount; i++){
     if (composite.imageCollection[composite.images[i]].complete) {
@@ -89,7 +91,12 @@ var tracker = setInterval(()=>{
       //so we have tmpTime
       let animTime = tmpTime / (composite.imageTimeStamps.length + 0.25)
       console.log('animTime: ', animTime)
-      document.documentElement.style.setProperty('--maskAnimDuration', animTime + 's')
+      if (presets.useCssTransition) {
+        //console.log('right')
+        document.documentElement.style.setProperty('--maskAnimDuration', animTime + 's')
+      } else {
+        document.documentElement.style.setProperty('--maskAnimDuration', 0)
+      }
       composite.imageTimeStamps.push(tmpTime)
       //console.log(`an image completed. At ${tmpTime - startTime}ms since start.`)
       
@@ -158,26 +165,21 @@ bar.classList.add('accubar')
 
 function initLoadingBar(){
   document.body.appendChild(bar)
+  composite.mask = document.getElementsByClassName('maskMovingPart')[0]
+  document.documentElement.style.setProperty('--ghostTime', presets.ghostTime + 'ms')
 }
 function barAnimateProgress(){
+  
   let perc = composite.realProgress / composite.progressCap//, animPerc = composite.animatedProgress / composite.progressCap
   document.getElementsByClassName('realProgressDebug')[0].textContent = 'real progress: ' + (perc * 100 | 0) + '%'
   document.getElementsByClassName('dbtxt2')[0].textContent = `document state: ${document.readyState} (${document.readyState == 'interactive' ? 2 : 3} / 3)`
   document.getElementsByClassName('imagesProgressDebug')[0].textContent = `imgs: ${composite.doneImages} / ${composite.imageCollection.length}${composite.images.length==0 ? ' done' : ''}`
   document.getElementsByClassName('dbtxt4')[0].textContent = 'img loading timestamps: ' + composite.imageTimeStamps.join('s ') + 's'
-  //document.getElementsByClassName('dbtxt4')[0].textContent = 'animated progress: ' + (animPerc * 100 | 0) + '%'
-  //document.getElementsByClassName('maskMovingPart')[0].setAttribute('x', perc* presets.svgWidth)
   if (presets.useCssTransition) {
-    //document.getElementsByClassName('maskMovingPart')[0].setAttribute('x', animPerc* presets.svgWidth)
-    //SO REPLACE THE ANIMATION TO CSS ONE??? ITS SMOOTHER
-    //JUST NEED TO ESTIMATE TIME AND USE THAT
-    document.getElementsByClassName('maskMovingPart')[0].style.setProperty('--maskPosX', perc* presets.svgWidth + 'px')
+    composite.mask.style.setProperty('--maskPosX', perc * presets.svgWidth + 'px')
   } else {
-    //maskPosX
-    document.getElementsByClassName('maskMovingPart')[0].setAttribute('x', perc* presets.svgWidth)
-    //
+    composite.mask.setAttribute('x', perc * presets.svgWidth)
   }
-  
   
   //if (perc >= 1) composite.animatedProgress += 0.1
 
@@ -185,8 +187,7 @@ function barAnimateProgress(){
   //let animationBase = presets.useSmoothing ? animPerc : perc 
   if (perc >= 1) {
     document.getElementsByClassName('documentTimeStampsDebug')[0].innerHTML += 'real progress 100%: ' + parseFloat((new Date().getTime() - startTime)/1000).toFixed(2) + 's<br>'
-    document.getElementsByClassName('maskMovingPart')[0].addEventListener('transitionend', () => {
-      //console.log('maskMovingPart transitionend')
+    composite.mask.addEventListener('transitionend', () => {
       document.getElementsByClassName('documentTimeStampsDebug')[0].innerHTML += 'animation finished at 100%: ' + parseFloat((new Date().getTime() - startTime)/1000).toFixed(2) + 's'
       document.getElementsByClassName('lastTipDebug')[0].textContent = `Дополнительное время до закрытия(ghost time): ${parseFloat(presets.ghostTime / 1000).toFixed(2)}s`
       document.getElementsByClassName('inner-svg')[0].classList.add('svgEndAnimation')
@@ -198,31 +199,18 @@ function barAnimateProgress(){
           if (e.target == bar) bar.style.display = 'none' //change this for some disappering effect  
         })
         
-        //clearInterval(animator)
         document.body.classList.remove('stop-scrolling')
         console.log('BAR TERMINATED')
       },presets.ghostTime)
     })
+    if (!presets.useCssTransition) {
+      let fakeTransitionEnd = document.createEvent('Events')
+      fakeTransitionEnd.initEvent('transitionend', true, false)
+      composite.mask.dispatchEvent(fakeTransitionEnd)
+    }
     //bar.classList.add('endAnimation')
     
   }
 }
 
-function animation(){
-  //make drawing of progress bar show animatedProgress
-  //that animated Progress - compute it from real progress
-  //also formulae of deriving - each timer tick add some of real progress to animated progress
-  //but it should end almost same as real 100% somehow
-  //for now just draw 2 percent per second instead of all in
-  
-  if (composite.animatedProgress < composite.realProgress) {
-    composite.animatedProgress += 0.05
-    
-  } else composite.animatedProgress = composite.realProgress
-}
-
-/*var animator = setInterval(()=>{
-  animation()
-},20)
-*/
 
